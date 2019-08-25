a54f4d80514fa7cf3bd8aeb3e78d99c8b4d0bad0
joan <sjlouji10@gmail.com>
2019-08-25 22:31:10 +0530

    adding a task and list complete

--- frontend/src/app/taskview/taskview.component.ts
+++ frontend/src/app/taskview/taskview.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from '../task.service';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-taskview',
@@ -8,21 +9,53 @@ import { TaskService } from '../task.service';
 })
 export class TaskviewComponent implements OnInit {
   isModalActive: boolean = false;
-  constructor(private taskService: TaskService) { }
+  isModalActiveTask: boolean = false;
 
+  constructor(private taskService: TaskService,private Route: ActivatedRoute) { }
+  lists: any[];
+  tasks: any[];
+  idoflist: any[];
   ngOnInit() {
+    this.Route.params.subscribe((params: Params)=>{
+        {
+          console.log(params);
+          this.taskService.getTask(params.listId).subscribe((tasks: any[])=>{
+            this.tasks = tasks;
+          });
+        }
+    });
+    this.taskService.getList().subscribe((lists: any[])=>{
+      this.lists = lists;
+      console.log(lists);
+   });
   }
   openDialog(){
       this.isModalActive = !this.isModalActive;
   }
-
+  openTaskDialog(){
+    this.isModalActiveTask = !this.isModalActiveTask;
+  }
   createnewList(title: String){
   this.taskService.createList(title).subscribe((response: any) =>{
       console.log(response);
       this.closeModelTask();
+      window.location.reload();
     });
   }
+
+  createnewListTask(title: String){
+    this.Route.params.subscribe((params: Params)=>{
+      this.taskService.createListTask(title,params.listId).subscribe((response: any) =>{
+        console.log(response);
+        this.closeModelTask();
+        window.location.reload();
+      });
+    });
+   
+    }
+
   closeModelTask(){
     this.isModalActive = false;
+    this.isModalActiveTask = false;
   }
 }
